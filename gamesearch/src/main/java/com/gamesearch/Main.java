763b3195a6bd7aa8f1f092f1876189f553c25d1e
Naoise <naoisehatton03@gmail.com>
2026-07-19 15:55:41 +0100

    built working http request to get the pricing information for the us release of dark souls remastered

--- gamesearch/src/main/java/com/gamesearch/Main.java
+++ gamesearch/src/main/java/com/gamesearch/Main.java
@@ -1,9 +1,19 @@
 package com.gamesearch;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
+
+    private static final String API_KEY = "hWbNrlwHAhsrGFMszkmcYLbHtvNWkQS0";
+    
+
     public static void main(String[] args) throws Exception {
 
         String rawJsonString = """
@@ -17,11 +27,40 @@ public class Main {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(rawJsonString);
 
-        System.out.println("Successfully parsed rawJsonString object");
-        System.out.println(root.get("title").asText());
-        System.out.println(root.get("price").asDouble());
-        System.out.println(root.get("tags").get(0).asText());
+        // System.out.println("Successfully parsed rawJsonString object");
+        // System.out.println(root.get("title").asText());
+        // System.out.println(root.get("price").asDouble());
+        // System.out.println(root.get("tags").get(0).asText());
 
-        System.out.println(root.toPrettyString());
+        // System.out.println(root.toPrettyString());
+
+        String url = String.format(
+            "https://api.gg.deals/v1/prices/by-steam-app-id/?ids=570940&key=%s&region=us",API_KEY
+        );
+
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Accept", "application/json")
+                .timeout(Duration.ofSeconds(10))
+                .GET()
+                .build();
+                
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Status code: " + response.statusCode());
+            System.out.println("Body: ");
+            System.out.println(response.body());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }        
+            
+
+
     }
 }
