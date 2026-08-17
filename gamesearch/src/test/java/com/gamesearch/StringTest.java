9cd21bf8377bb793f020ae68ce13f84dc6b58332
Naoise <naoisehatton03@gmail.com>
2026-08-17 18:16:47 +0100

    updated the xml file to allow for parameterized tests and streamlined string test to the the tested method is only called once but the given values are changed each time.

--- gamesearch/src/test/java/com/gamesearch/StringTest.java
+++ gamesearch/src/test/java/com/gamesearch/StringTest.java
@@ -1,32 +1,31 @@
 package com.gamesearch;
+import java.util.stream.Stream;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.runners.Parameterized;
 
 public class StringTest {
 
-    @Test
-    public void testCreateIdsString(){
+    static Stream<Arguments>provideTestCases(){
+        return Stream.of(
+          Arguments.of(new String[]{}, ""),
+          Arguments.of(new String[]{"test"}, "test"),
+          Arguments.of(new String[]{"one", "two"}, "one,two")   
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    public void testCreateIdsString(String[] actual, String expected){
         //Arrange
         GGDealsApi api = new GGDealsApi("Dummy Key");
         //Act
-        var actual = api.createIdsString(new String[]{});
-        var expected = "";
+        assertEquals(expected, api.createIdsString(actual));
+        
 
-        assertEquals(expected, actual);
-
-        actual = api.createIdsString(new String[]{null});
-        expected = "null";
-
-        assertEquals(expected, actual);
-
-        actual = api.createIdsString(new String[]{"test"});
-        expected = "test";
-
-        assertEquals(expected, actual);
-
-        actual = api.createIdsString(new String[]{"one", "two"});
-        expected = "one,two";
-
-        assertEquals(expected, actual);
+       
     }
 }
